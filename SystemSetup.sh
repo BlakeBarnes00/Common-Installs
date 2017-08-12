daf10ef4d55901da16a9d88997d1ddf930343dc0
Ellis Barnes <ellisbarnes00@gmail.com>
2017-08-12 14:06:41 -0400

    Added quotes to the words, moved synaptic to tools

--- SystemSetup.sh
+++ SystemSetup.sh
@@ -1,37 +1,40 @@
 #!/bin/sh
 
 # PPA's
+sleep 2 && echo "Adding repositories..."
 sudo apt-add-repository ppa:graphics-drivers/ppa -y
 sudo apt-add-repository ppa:numix/ppa -y
 
 sudo apt-get update # Update the PPA's
 
 # Developer basics:
-sudo apt-get install synaptic -y  # Package management
-sudo apt-get install git -y       # Version control
-sudo apt-get install vim -y       # File editing
-sudo apt-get install premake4 -y  # Makefile creator
+sleep 2 && echo "Adding developer tools../"
+sudo apt-get install "git" -y       # Version control
+sudo apt-get install "vim" -y       # File editing
+sudo apt-get install "premake4" -y  # Makefile creator
+sudo apt-get install "lua5.3" -y    # Lua files
 
 # Networking
-sudo apt-get install nmap -y            # Scan wifi
-sudo apt-get install net-tools -y       # Basic networking tools
-sudo apt-get install openssh-server -y  # Enable SSH on this device
+sudo apt-get install "nmap" -y            # Scan wifi
+sudo apt-get install "net-tools" -y       # Basic networking tools
+sudo apt-get install "openssh-server" -y  # Enable SSH on this device
 
 # Drivers
-sudo apt-get remove --purge nvidia* -y  # Remove all nvidea drivers
-sudo apt-get install nvidia-384 -y      # This is the most recent driver
+sudo apt-get remove --purge nvidia* -y    # Remove all nvidea drivers
+sudo apt-get install "nvidia-384" -y      # This is the most recent driver
 
 # Tools
-sudo apt-get install nmap -y              # Network scanning
-sudo apt-get install unity-tweak-tool -y  # Manage themes
-sudo apt-get install net-tools -y         # Helps show network properties
+sudo apt-get install "synaptic" -y          # Package management
+sudo apt-get install "nmap" -y              # Network scanning
+sudo apt-get install "unity-tweak-tool" -y  # Manage themes
+sudo apt-get install "net-tools" -y         # Helps show network properties
 
 # Themes
-sudo apt-get install numix-gtk-theme numix-icon-theme-circle -y
+sudo apt-get install "numix-gtk-theme" "numix-icon-theme-circle" -y
 
 # Update and close Ubuntu
-sudo apt-get upgrade
+sudo apt-get upgrade -y
 sudo apt-get update -y
 
-echo "Rebooting machine... Please stand by :)"
+sleep 10 && echo "Rebooting machine... Please stand by :) in 10 seconds!"
 sudo reboot --reboot

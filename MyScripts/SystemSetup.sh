83f995bc36d898f02e18487346b866e9558c19be
Ellis Barnes <ellisbarnes00@gmail.com>
2017-08-17 23:07:41 -0400

    Added options to the file

--- MyScripts/SystemSetup.sh
+++ MyScripts/SystemSetup.sh
@@ -1,51 +1,70 @@
 #!/bin/sh
 
-# PPA's
-sleep 2 && echo "Adding repositories..."
-sudo apt-add-repository ppa:graphics-drivers/ppa -y
-sudo apt-add-repository ppa:numix/ppa -y
-sudo apt-add-repository ppa:webupd8team/java -y
+if [ "$1" = "help" ]
+then
+  echo "themes            - Install theme manager and themes"
+  echo "developer-tools   - Install developer tools"
+  echo "graphics-drivers  - Install Nvidia Graphics Drivers"
+  echo "networking        - Install networking tools"
+  echo "misc              - Install misc. tools"
+fi
 
+if [ "$1" = "themes" ]
+then
+  sleep 2 && echo "Added Numix repository and downloading..."
+  sudo apt-add-repository ppa:numix/ppa -y
+  sudo apt-get update # Update the PPA list
 
-sudo apt-get update # Update the PPA's
+  sudo apt-get install "numix-gtk-theme" -y
+  sudo apt-get install "numix-icon-theme-circle" -y
+  sudo apt-get install "numix-folders" -y
 
-# Developer basics:
-sleep 2 && echo "Adding developer tools.."
-sudo apt-get install "git" -y       # Version control
-sudo apt-get install "vim" -y       # File editing
-sudo apt-get install "premake4" -y  # Makefile creator
-sudo apt-get install "lua5.3" -y    # Lua files
-sudo apt-get install "python3.6"    # Most recent python version
-sudo apt-get install "oracle-java9-installer" -y  # Java 9 JDK
+  # Now install the unity tweak tools
+  sudo apt-get install "unity-tweak-tool" -y
 
-# Networking
-sleep 2 && echo "Adding Networking tools.."
-sudo apt-get install "nmap" -y            # Scan wifi
-sudo apt-get install "net-tools" -y       # Basic networking tools
-sudo apt-get install "openssh-server" -y  # Enable SSH on this device
+elif [ "$1" = "developer-tools" ]
+then
+  sleep 2 && echo "Installing developer stuff..."
+  sudo apt-get install "git" -y
+  sudo apt-get install "vim" -y
+  sudo apt-get install "premake4" -y
+  sudo apt-get install "lua5.3" -y
+  sudo apt-get install "python3.6" -y
+  
+  # This is for Java development
+  sleep 2 && echo "Adding Oracle repository for Java and downloading..."
+  sudo apt-add-repository ppa:webupd8team/java -y
+  sudo apt-get update # Update the PPA list
+  sudo apt-get install "oracle-java9-installer" -y
 
-# Drivers
-sleep 2 && echo "Setting up Nvidia drivers tools.."
-sudo apt-get remove --purge nvidia* -y    # Remove all nvidea drivers
-sudo apt-get install "nvidia-384" -y      # This is the most recent driver
+elif [ "$1" = "graphics-drivers" ]
+then
+  sleep 2 && echo "Adding Nvidea graphics repository and downloading..."
+  sudo apt-add-repository ppa:graphics-drivers/ppa -y
+  sudo apt-get update # Update the PPA list
+  
+  sudo apt-get purge nvidia* -y # Remove all Nvidia drivers (if any)
+  sudo apt-get install "nvidia-384" -y # Most recent drivers
 
-# Tools
-sleep 2 && echo "Adding other misc. tools.."
-sudo apt-get install "synaptic" -y          # Package management
-sudo apt-get install "nmap" -y              # Network scanning
-sudo apt-get install "unity-tweak-tool" -y  # Manage themes
-sudo apt-get install "net-tools" -y         # Helps show network properties
+elif [ "$1" = "networking" ]
+then
+  sleep 2 && echo "Adding Networking tools.."
+  sudo apt-get install "nmap" -y            # Scan wifi
+  sudo apt-get install "net-tools" -y       # Basic networking tools
+  sudo apt-get install "openssh-server" -y  # Enable SSH on this device
 
-# Themes
-sleep 2 && echo "Adding themes.."
-sudo apt-get install "numix-gtk-theme" -y 
-sudo apt-get install "numix-icon-theme-circle" -y
-sudo apt-get install "numix-folders" -y
+elif [ "$1" = "misc" ]
+then
+  sudo apt-get install "synaptic" -y          # Package management
+fi
 
+if [ "$2" = "-u" ]
+then
+  sudo apt-get upgrade -y
+  sudo apt-get update -y
 
-# Update and close Ubuntu
-sudo apt-get upgrade -y
-sudo apt-get update -y
-
-sleep 10 && echo "Rebooting machine... Please stand by :) in 10 seconds!"
-sudo reboot --reboot
+elif [ "$2" = "-r" ]
+then
+  sleep 10 && echo "Rebooting machine... Please stand by :) in 10 seconds!"
+  sudo reboot --reboot
+fi
